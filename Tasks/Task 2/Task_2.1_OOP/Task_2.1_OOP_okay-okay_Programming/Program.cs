06e84d15bc63617e7ba4ea5bc2d3272637ca5217
ShinnikovVA <mr.shchinnikov@mail.ru>
2022-01-20 17:07:10 +0400

    UI is done

--- Tasks/Task 2/Task_2.1_OOP/Task_2.1_OOP_okay-okay_Programming/Program.cs
+++ Tasks/Task 2/Task_2.1_OOP/Task_2.1_OOP_okay-okay_Programming/Program.cs
@@ -12,18 +12,48 @@ namespace Task_2._1_OOP_okay_okay_Programming
             {
                 switch (menu)
                 {
-                    case "1":
+                    case "1": // Работа со строками
                         {
                             var strMenu = StringMenu();
                             switch (strMenu)
                             {
-                                case "1":
+                                case "1": // Сравнение
                                     {
                                         Console.WriteLine();
                                         strMenu = StringMenu();
                                         break;
                                     }
-                                case "0":
+                                case "2": // Конкатенация
+                                    {
+                                        Console.WriteLine();
+                                        strMenu = StringMenu();
+                                        break;
+                                    }
+                                case "3": //  Поиск символов
+                                    {
+                                        Console.WriteLine();
+                                        strMenu = StringMenu();
+                                        break;
+                                    }
+                                case "4": // Конвертация массива символов
+                                    {
+                                        Console.WriteLine();
+                                        strMenu = StringMenu();
+                                        break;
+                                    }
+                                case "5": //
+                                    {
+                                        Console.WriteLine();
+                                        strMenu = StringMenu();
+                                        break;
+                                    }
+                                case "6": //
+                                    {
+                                        Console.WriteLine();
+                                        strMenu = StringMenu();
+                                        break;
+                                    }
+                                case "0": // Назад
                                     {
                                         break;
                                     }
@@ -40,8 +70,42 @@ namespace Task_2._1_OOP_okay_okay_Programming
                             menu = MainMenu();
                             break;
                         }
-                    case "2":
+                    case "2": // Работа с фигурами
                         {
+                            var figMenu = FigureMenu();
+                            switch (figMenu)
+                            {
+                                case "1": // Добавить фигуру
+                                    {
+                                        Console.WriteLine();
+                                        figMenu = FigureMenu();
+                                        break;
+                                    }
+                                case "2": // Показать все фигуры
+                                    {
+                                        Console.WriteLine();
+                                        figMenu = FigureMenu();
+                                        break;
+                                    }
+                                case "3": // Удалить все фигуры
+                                    {
+                                        Console.WriteLine();
+                                        figMenu = FigureMenu();
+                                        break;
+                                    }
+                                case "0": // Назад
+                                    {
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        Console.WriteLine("Неверный вариант!");
+                                        Console.WriteLine();
+                                        figMenu = FigureMenu();
+                                        break;
+                                    }
+                            }
+
                             Console.WriteLine();
                             menu = MainMenu();
                             break;
@@ -86,6 +150,11 @@ namespace Task_2._1_OOP_okay_okay_Programming
 
         static string FigureMenu()
         {
+            Console.WriteLine("Выберите действие с фигурами: ");
+            Console.WriteLine(" 1 - Добавить фигуру");
+            Console.WriteLine(" 2 - Показать все фигуры");
+            Console.WriteLine(" 3 - Удалить все фигуры");
+            Console.WriteLine(" 0 - Назад");
             return Console.ReadLine();
         }
     }
