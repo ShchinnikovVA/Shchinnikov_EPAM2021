09700eb0f90c8807d41739302f275ad9276b56df
ShinnikovVA <mr.shchinnikov@mail.ru>
2022-01-27 16:12:27 +0400

    что я делаю

---
+++ Tasks/DLL/EpamClassLibrary/EpamClassLibrary/StringIsCharArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace EpamClassLibrary
+{
+    public static class StringIsCharArray
+    {
+        public static bool Comparison(string str1, string str2) // Принимает 2 строки
+        {
+            bool equability = true; // Изначально булевая тру
+            
+            char[] ch1 = str1.ToCharArray(); // Создаём 2 массива символов
+            char[] ch2 = str2.ToCharArray();
+            int i = 0;
+
+            while (i < ch1.Length)
+            {
+                if (ch1[i] == ch2[i]) i++; // Сравниваем символы в массивах
+                else
+                {
+                    equability = false; // Если не равны то меняем булевую 
+                    break;
+                }
+            }
+
+            return equability;
+        }
+
+        public static string Concatenation(string str1, string str2)
+        {
+            StringBuilder sb = new StringBuilder(str1); // создаём стрингбилдет с первй строкой
+            sb.Append(str2); // добавляем вторую
+            return sb.ToString();
+        }
+
+        public static string ArrayToString()
+        {
+            char[] chars = new char[Numbers.Read_Int("Введите длину массива: ")];
+            for (int i = 0; i < chars.Length; i++)
+            {
+                Console.WriteLine("Введите символ [{0}]: ", i+1);
+                chars[i] = Convert.ToChar(Console.ReadLine()[0]); //Считывается только первый символ строки
+            }
+            return new string(chars);
+        }
+
+        //public static char[] StringToArray(string str)
+        //{
+        //    return str.ToCharArray();
+        //}
+       
+    }
+}
---
+++ Tasks/Task 2/Task_2.1_OOP/Task_2.1_OOP_okay-okay_Programming/StringForThisTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2._1_OOP_okay_okay_Programming
+{
+    class StringForThisTask
+    {
+        public static void GoFindChar(string str1, string str2)
+        {
+            char[] ch1 = str1.ToCharArray(); // Создаём 2 массива символов
+            char[] ch2 = str2.ToCharArray();
+
+            for (int i = 0; i < ch2.Length; i++)
+            {
+                int amount = 0;
+                for (int j = 0; j < ch1.Length; j++)
+                {
+                    if (ch2[i] == ch1[j])
+                    {
+                        amount++;
+                    }
+                }
+                if (amount > 0)
+                    Console.WriteLine("Количество символов '{0}': {1}", ch2[i], amount);
+                else
+                    Console.WriteLine("Символа '{0}' не найдено!", ch2[i]);
+
+            }
+        }
+
+        public static string ThatSaidVladimir()
+        {
+            Console.WriteLine("Это почти как мой говорящий Томас");
+            Console.WriteLine("Введите предложение, которое скажет Владимир:");
+            StringBuilder str = new StringBuilder(Console.ReadLine());
+            str.Append(" - Сказал Владимир.");
+            return str.ToString();
+
+        }
+    }
+}
