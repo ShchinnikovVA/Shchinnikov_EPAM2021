65e931c97a6c0d541c08df5c09e3fad96e3dc8c2
ShinnikovVA <mr.shchinnikov@mail.ru>
2022-02-08 08:44:39 +0400

    dynamic array class billet created

---
+++ Tasks/Task 3/DynamicArray/DynamicArray/ArrayDynamic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicArray
+{
+    public class ArrayDynamic<T>
+    {
+        private T[] arr;
+        public int lenght { get; private set; } //получение количества элементов. Не путать с ёмкостью (Capacity).
+        public int capacity { get; private set; } //получение ёмкости: длины внутреннего массива.
+        public ArrayDynamic()
+        {
+            arr = new T[8];
+        }
+        public ArrayDynamic(int lenght)
+        {
+            arr = new T[lenght];
+        }
+
+        protected void Add(T item)
+        {
+
+        }
+
+        protected void AddRange()
+        {
+
+        }
+
+        protected bool Remove(int index)
+        {
+            return true;
+        }
+
+        protected bool Insert(int index, T item)
+        {
+
+            return true;
+        }
+    }
+}
---
+++ Tasks/Task 3/DynamicArray/DynamicArray/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EpamClassLibrary;
+
+namespace DynamicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
